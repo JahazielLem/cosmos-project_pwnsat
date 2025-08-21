cef6737977912d226603f1ff133bb07762ef15c8
Kevin Jahaziel Leon Morales <jahaziel.lem@gmail.com>
2025-08-21 10:15:08 -0600

    Refactor: Commands and TM

--- CONNECTOR/sat.py
+++ CONNECTOR/sat.py
@@ -1,46 +1,87 @@
+"""
+v0.0.1.0 - Initial base code
+v0.0.2.0 - Change serial configuration
+"""
 import socket
 import serial
 import struct
 import threading
 import time
 
-SOFTWARE_VERSION = "0.0.1"
+SOFTWARE_VERSION = "0.0.2.0"
+BINARY_START_BYTE = 0x7E
 
 LISTEN_IP = "0.0.0.0"
-COMMAND_PORT = 1236
+COMMAND_PORT = 1235
 TELEMETRY_PORT = 1234
-TELEMETRY_IP = "cosmos-project-openc3-operator-1"
+TELEMETRY_IP = "cosmos-project_pwnsat-openc3-operator-1"
 
 satellite_mode = "NORMAL"
 
-ser = serial.Serial("/dev/tty.usbmodem2123401", 115200)
+ser = serial.Serial("/dev/ttyACM0", 115200)
 
 sock_command = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock_command.bind((LISTEN_IP, COMMAND_PORT))
 
 sock_telemetry = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+#define APID_TM_PING_ACK 201     // Response <- APID_TC_PING_SYNC
+#define APID_TM_SEND_STATUS 450  // Response <- APID_TC_GET_STATUS
+#define APID_TM_SEND_TEMP 451    // Response <- APID_TC_GET_TEMP
+#define APID_TM_SEND_GYRO 452    // Response <- APID_TC_GET_GYRO
+#define APID_TM_SEND_TM 453      // Response <- APID_TC_GET_TM
+
 tlm_ids = {
-    "ERROR"   : 0x4320,
-    "STATUS"  : 0x4321,
-    "PING"    : 0x4322,
-    "GET_TM"  : 0x4323,
-    "GET_TEMP": 0x4324,
-    "GET_GYRO": 0x4325
+    "ERROR": 0x4320,
+    "STATUS": 0x4321,
+    "PING": 0x4322,
+    "TM": 0x4323,
+    "TEMP": 0x4324,
+    "GYRO": 0x4325,
+    "IDLE": 0x4330
 }
 
 tlm_map = {
-    "STATUS": "get_config",
-    "PING"  : "ping",
+    "PING"  : "send_ping",
+    "STATUS": "get_status",
+    "GET_TEMP": "get_temp",
+    "GET_GYRO": "get_gyro",
     "GET_TM": "get_tm",
     "ERROR" : 0x4320
 }
+
+def send_gs_tc_response(command, message):
+    id = tlm_ids[command]
+    fmt = f'>h{len(message)}s'
+    packed = struct.pack(fmt, id, message)
+    sock_telemetry.sendto(packed, (TELEMETRY_IP, TELEMETRY_PORT))
+    print(f"Sending TC response: {message}")
+
+
+def send_gs_serial_cmd(cmd):
+    ser_cmd = f"{tlm_map[cmd]}\r\n"
+    ser.write(ser_cmd.encode())
 
 def recv_worker():
     while True:
         try:
             data = ser.readline()
-            print(f"[SERIAL] Recv -> {data}")
+            if b"@;" in data:
+                apid = data.split(b"@;")[0].replace(b"\r\n", b"")
+                print(f"APID: {apid}")
+                data = data.split(b"@;")[1].replace(b"\r\n", b"")
+                if int(apid) == 201:
+                    send_gs_tc_response("PING", f"PING:{data.decode()}".encode())
+                elif int(apid) == 450:
+                    send_gs_tc_response("STATUS", f"STATUS:{data.decode()}".encode())
+                elif int(apid) == 451:  
+                    send_gs_tc_response("TEMP", data)
+                elif int(apid) == 452:
+                    send_gs_tc_response("GYRO", data)
+                elif int(apid) == 453 or int(apid) == 100:
+                    send_gs_tc_response("TM", data)
+                elif int(apid) == 100:
+                    send_gs_tc_response("TM", data)
             time.sleep(0.1)
         except KeyboardInterrupt:
             ser.close()
@@ -51,56 +92,22 @@ print("Satellite Ground Station Connector")
 print(f"Version: {SOFTWARE_VERSION}")
 print("Waiting for commands...")
 
-def send_gs_tc_response(command, message):
-    id = tlm_ids[command]
-    fmt = '>h12s'
-    packed = struct.pack(fmt, id, message.encode("latin1"))
-    sock_telemetry.sendto(packed, (TELEMETRY_IP, TELEMETRY_PORT))
-    print(f"Sending TC response: {message}")
-
-
 threading.Thread(target=recv_worker, daemon=True).start()
 
 while True:
     try:
         data, addr = sock_command.recvfrom(1024)
-        print(data)
         try:
             command = struct.unpack_from(">b", data)[0]
-            print(f"[{addr}] Received command: {command}")
+            print(f"Command: {command}")
             if(command == 0):
                 payload = struct.unpack_from(">f", data[1:])[0]
                 send_gs_tc_response("STATUS", f"STATUS:FREQ:OK:{payload}")
-            elif(command == 1):
-                payload = struct.unpack_from(">f", data[1:])[0]
-                send_gs_tc_response("STATUS", f"STATUS:BW:OK:{payload}")
-            elif command == 2:
-                payload = struct.unpack_from(">i", data[1:])[0]
-                send_gs_tc_response("STATUS", f"STATUS:SP:OK:{payload}")
-            elif command == 3:
-                payload = struct.unpack_from(">i", data[1:])[0]
-                send_gs_tc_response("STATUS", f"STATUS:CR:OK:{payload}")
-            elif command == 4:
-                payload = struct.unpack_from(">i", data[1:])[0]
-                send_gs_tc_response("STATUS", f"STATUS:PL:OK:{payload}")
             else:
                 str_cmd = data.decode('utf-8')
-                if str_cmd == "PING":
-                    send_gs_tc_response(str_cmd, "PING:ACK")
-                elif str_cmd == "STATUS":
-                    send_gs_tc_response(str_cmd, "STATUS:OK")
-                elif str_cmd == "MODE":
-                    send_gs_tc_response(str_cmd, "MODE:OK")
-                elif str_cmd == "GET_TEMP":
-                    send_gs_tc_response(str_cmd, "TEMP:OK")
-                elif str_cmd == "GET_GYRO":
-                    send_gs_tc_response(str_cmd, "GYRO:OK")
-                elif str_cmd == "GET_TM":
-                    send_gs_tc_response(str_cmd, "TM:OK")
-                else:
-                    send_gs_tc_response("ERROR", "UNKNOWN_CMD")
-
+                send_gs_serial_cmd(str_cmd)
         except Exception as e:
             print(e)
     except KeyboardInterrupt:
+        ser.close()
         break
